dba310ecf267f926dfa0a5d6ad8718c9f00408d8
hrisa006 <hrisi_karaivanova@abv.bg>
2025-02-12 05:27:56 +0200

    KK

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
-const express = require('express');
-const ejs = require('ejs');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require("express");
+const ejs = require("ejs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
 const PORT = 8080;
 
 app.use(cookieParser());
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 function authenticateToken(req, res, next) {
-    const authCookie = req.cookies['auth_cookie'];
+  const authCookie = req.cookies["auth_cookie"];
 
-    if(!authCookie) {
-        res.sendStatus(401);
+  if (!authCookie) {
+    res.sendStatus(401);
+  }
+
+  jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      res.sendStatus(403);
     }
-
-    jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) {
-            res.sendStatus(403);
-        }
-        req.user = user;
-        next();
-    });
+    req.user = user;
+    next();
+  });
 }
 
-app.get('/auth/me', authenticateToken, (req, res) => {
-    res.json(req.user);
+app.get("/auth/me", authenticateToken, (req, res) => {
+  res.json(req.user);
 });
 
-app.post('/auth/register', (req, res) => {
-    const {email, username, password} = req.body;
+app.post("/auth/register", (req, res) => {
+  const { email, username, password } = req.body;
 
+  const token = jwt.sign({ email, username }, process.env.ACCESS_TOKEN_SECRET);
+  res.cookie("auth_cookie", token, { maxAge: 900000, httpOnly: true });
 
-
-    const token = jwt.sign({email, username}, process.env.ACCESS_TOKEN_SECRET);
-    res.cookie('auth_cookie', token, {maxAge:900000, httpOnly: true});
-
-    res.status(201).json({message: 'User created', user: {email, username}});
-    // res.render('index');
+  res.status(201).json({ message: "User created", user: { email, username } });
+  // res.render('index');
 });
 
-app.get('/auth/login', (req, res) => {
-    const {email, password} = req.body;
-    const token = jwt.sign({email, username}, process.env.ACCESS_TOKEN_SECRET);
-    res.cookie('auth_cookie', token, {maxAge:900000, httpOnly: true});
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if(err) {
-            res.sendStatus(403);
-        }
-        res.json({user});
-    });
+app.get("/auth/login", (req, res) => {
+  const { email, password } = req.body;
+  const token = jwt.sign({ email, username }, process.env.ACCESS_TOKEN_SECRET);
+  res.cookie("auth_cookie", token, { maxAge: 900000, httpOnly: true });
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      res.sendStatus(403);
+    }
+    res.json({ user });
+  });
 });
 
-app.listen(PORT,  () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
--- server.js
+++ server.js
@@ -1,30 +1,59 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
 
 const app = express();
+const PORT = 8080;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.json());
 
 const posts = [
-    {
-        username: 'Hrisi',
-        title: 'Post 1'
-    },
-    {
-        username: 'Pisi',
-        title: 'Post 2'
-    }
+  {
+    username: "Hrisi",
+    title: "Post 1",
+  },
+  {
+    username: "Pisi",
+    title: "Post 2",
+  },
 ];
 
-app.get('/posts', (req, res) => {
-    res.json(posts);
+app.get("/posts", (req, res) => {
+  res.json(posts);
 });
 
-app.post('/posts', (req, res) => {
-    const user = {username: req.body.username, password: req.body.password};
+app.post("/posts", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { username: req.body.username, password: hashedPassword };
     posts.push(user);
     res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
 });
 
+app.post("/posts/login", async (req, res) => {
+  const user = posts.find((user) => user.username === req.body.username);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Not allowed");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 
-app.listen(8080);
+app.listen(PORT);
